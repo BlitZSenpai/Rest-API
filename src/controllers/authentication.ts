38723a9381e915bcd7c0a2fc2b7a6b74e8884080
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-06 00:23:23 +0530

    bug fixes

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -5,14 +5,14 @@ import { random, authentication } from "../helpers/index";
 
 export const login = async (req: express.Request, res: express.Response) => {
   try {
-    const [ email, password ] = req.body;
+    const { email, password } = req.body;
 
+    
     if(!email || !password) {
       return res.sendStatus(400);
     };
 
     const user = await getUserByEmail(email).select("+authentication.salt +authentication.password");
-
     if(!user) {
       return res.sendStatus(400);
     };
@@ -46,13 +46,12 @@ export const register = async (req: express.Request, res: express.Response) => {
     }
 
     const existingUser = await getUserByEmail(email);
-
     if(existingUser) {
       return res.sendStatus(400);
     }
 
     const salt = random();
-    const user = createUser({
+    const user = await createUser({
       username,
       email,
       authentication: {
